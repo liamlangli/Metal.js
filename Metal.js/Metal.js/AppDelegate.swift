3bc8ab67c2a7dccfb30432ccb285821593452714
lang <lilang8936@gmail.com>
2022-07-19 20:47:03 +0800

    remove metalpp

---
+++ Metal.js/Metal.js/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Metal.js
+//
+//  Created by Lang on 2022/7/19.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
